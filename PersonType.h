2614df29ef160d026583597528257c275c998de7
1mac1400 <167825555+1mac1400@users.noreply.github.com>
2024-04-22 23:28:43 -0700

    Update PersonType.h

--- PersonType.h
+++ PersonType.h
@@ -1,1 +1,39 @@
 
+
+
+#ifndef PERSONTYPE_H
+#define PERSONTYPE_H
+class PersonType
+{
+    //public interface
+    public:
+    
+    //default constructor
+    PersonType();
+    
+    //Overloaded constructor
+    
+    //Destructor
+    ~PersonType();
+    
+    //getters or accessors
+    string getFirstName();
+    string getLastName();
+    int getSSN();
+    
+    
+    //setters or mutators
+    void setPersonInfo(std::string firstName, std::string lastName, int newSSN);
+    
+    //other functionality
+    void printName();
+    void printPersonInfo();
+    void printSSN();
+    
+    private:
+    string fName;
+    string iName;
+    int SSN;
+    
+};
+#endif
