affb1018166cad0de7c3f00a62b90cc4449f6fca
1mac1400 <167825555+1mac1400@users.noreply.github.com>
2024-04-22 23:31:41 -0700

    Update PersonType.cpp

--- PersonType.cpp
+++ PersonType.cpp
@@ -1,2 +1,55 @@
-balls
-yo
+#include "PersonType.h"
+
+
+//default constructor
+PersonType::PersonType()
+{
+    SSN = 0;
+}
+
+//Overloaded constructor
+
+//Destructor
+PersonType::~PersonType()
+{
+    
+}
+
+//getters or accessors
+std::string PersonType::getFirstName()
+{
+    return fName;
+}
+std::string PersonType::getLastName()
+{
+    return iName;
+}
+int PersonType::getSSN()
+{
+    return SSN;
+}
+
+//setters or mutators
+void PersonType::setPersonInfo(std::string firstName, std::string lastName, int newSSN);
+{
+    fName = firstName;
+    iName = lastName;
+    SSN = newSSN;
+}
+
+
+//other functionality
+void PersonType::printName()
+{
+  
+}
+
+void PersonType::printPersonInfo()
+{
+    
+}
+
+void PersonType::printSSN()
+{
+    
+}
