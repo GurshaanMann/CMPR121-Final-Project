2b9229464aaf8d720402f020094e149518c3bbc2
1mac1400 <167825555+1mac1400@users.noreply.github.com>
2024-04-24 01:11:58 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
-#include <iostream>
+#include "PersonType.h"
+
 using namespace std;
 
 int main()
 {
-
-
+    PersonType user1;
+    string firstName;
+    string lastName;
+    double number;
+    
+    cout << "Enter your first and last name";
+    cin >> firstName >> lastName;
+    cout << "Enter your SSN";
+    cin >> number;
+    
+    user1.setPersonInfo(firstName, lastName, number);
+    
+    user1.printName();
+    user1.printPersonInfo();
+    user1.printSSN();
+    
 }
