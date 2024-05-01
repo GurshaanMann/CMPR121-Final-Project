1edb57919b803030eef987a1f85127d3f6ae1fd5
hemorrhage <167825934+gfloress1@users.noreply.github.com>
2024-05-01 16:20:04 -0700

    Update CandidateType.cpp

--- CandidateType.cpp
+++ CandidateType.cpp
@@ -1,1 +1,42 @@
+#include "CandidateType.h"
 
+CandidateType::CandidateType()
+{
+	totalVotes = 0;
+	array[NUM_OF_CAMPUSES] = {};
+}
+
+CandidateType::~CandidateType()
+{
+
+}
+
+int CandidateType::updateVotesByCampus(const int NUM_OF_CAMPUSES, int totalVotes)
+{
+
+}
+
+int CandidateType::getTotalVotes()
+{
+	return totalVotes;
+}
+
+int CandidateType::getVotesByCampus()
+{
+	return 
+}
+
+void CandidateType::printCandidateInfo()
+{
+
+}
+
+void CandidateType:: printTotalCandidateVotes()
+{
+
+}
+
+void CandidateType::printCandidateCampusVotes(const int NUM_OF_CAMPUSES)
+{
+
+}
