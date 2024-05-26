c7c432e702a3dbdcb031d9a792f7aabdbad427f2
1mac1400 <167825555+1mac1400@users.noreply.github.com>
2024-05-25 23:40:51 -0700

    Update CandidateList.h

--- CandidateList.h
+++ CandidateList.h
@@ -1,4 +1,9 @@
-
+/*
+ 
+ header
+ 
+ 
+ */
 #pragma once
 #ifndef CandidateList_h
 #define CandidateList_h
@@ -10,7 +15,7 @@ public:
     Node() : link(nullptr) {}
     Node(const CandidateType& votes, Node* theLink)
         : candidate(votes), link(theLink) {}
-    Node* getLink() const { return link; }
+    Node* getLink() const { return link; } 
     CandidateType getCandidate() const { return candidate; }
     void setCandidate(const CandidateType& votes) { candidate = votes; }
     void setLink(Node* theLink) { link = theLink; }
@@ -20,21 +25,22 @@ private:
     Node* link;        //pointer that points to next node
 };
 
-class CandidateList //potentially inherit CandidateType
+class CandidateList
 {
     public:
     CandidateList(); // constructor
     void addCandidate(const CandidateType& newCandidate); //creates a new node that stores object from candidateType class.
     int getWinner() const; //Checks to see which node has highest total votes and awards it as winner (aaccessor)
-    bool searchCandidate(int SSN); //Uses the parameter to search for candidate and prints there SSN
-    void printCandidateName(int SSN); //Uses the parameter to search for candidate and then prints there name
-    void printAllCandidates(); //Passes through each node printing the candidates name.
-    void printCandidateCampusVotes(int SSN, int divisonNumber); //
-    void printCandidateTotalVotes(int SSN); //Uses the parameter to search for candidate and then print there total votes
+    bool searchCandidate(int SSN) const; //Uses the parameter to search for candidate and prints there SSN
+    void printCandidateName(int SSN) const; //Uses the parameter to search for candidate and then prints there name
+    void printAllCandidates() const; //Passes through each node printing the candidates name.
+    void printCandidateCampusVotes(int SSN, int divisonNumber) const; //
+    void printCandidateTotalVotes(int SSN) const; //Uses the parameter to search for candidate and then print there total votes
     void destroyList(); //destroys node list and is called in the destructor
     ~CandidateList();//destructor
     //CandidateList(const CandidateList& object); //copy constructor
     //CandidateList& operator=(const CandidateList& rightSide); //overloaded assignment operator.
+    void printFinalResults() const;
     private:
     Node *first; //points to the first node and will be used for the start of list traversal
     Node *last; //points to last node (null)
